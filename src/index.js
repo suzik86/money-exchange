75786cd48b94e64d23d577af375a1567daf7effd
Irina Shishkova <i.suzanovich@mail.ru>
2019-03-14 16:09:24 +0300

    fix: solution optimization

--- src/index.js
+++ src/index.js
@@ -1,28 +1,23 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
     let result = {};
-    if (currency <= 0){
-        return {};
-    }
-    if (currency > 10000){
-        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
-    }
-    if (Math.floor(currency / 50) > 0){
-        result["H"] = Math.floor(currency / 50);        
-    }
-    if (Math.floor((currency%50) / 25) > 0){
-        result["Q"] = Math.floor(currency%50 / 25);
-    }
-    if (Math.floor(((currency%50)%25) / 10) > 0){
-        result["D"] = Math.floor(((currency%50)%25) / 10);
-    }
-    if (Math.floor((((currency%50)%25)%10) / 5) > 0){
-        result["N"] = Math.floor((((currency%50)%25)%10) / 5);
-    }
-    if (Math.floor((((currency%50)%25)%10)%5) > 0){
-        result["P"] = Math.floor(currency%50%25%10%5);
-    }
-    
+    let h = Math.floor(currency / 50);
+    let restH = currency%50;
+    let q = Math.floor(restH / 25);
+    let restQ = restH%25;
+    let d = Math.floor(restQ / 10);
+    let restD = restQ%10;
+    let n = Math.floor(restD / 5);
+    let restN = restD%5;
+    let p = Math.floor(restN);
 
+    if (currency <= 0) {return {};}
+    if (currency > 10000) {return {error: "You are rich, my friend! We don't have so much coins for exchange"};}
+    if (h > 0) {result["H"] = h;}
+    if (q > 0) {result["Q"] = q;}
+    if (d > 0) {result["D"] = d;}
+    if (n > 0) {result["N"] = n;}
+    if (p > 0) {result["P"] = p;}
+    
     return result;
 };
